1c5a54554000ee358653636ae7bd8013027e1459
KC <kc995266@gmail.com>
2024-05-27 00:12:23 +0800

    排版練習 (1/2) - SwiftUI 新手入門

---
+++ PracticeSwiftUI/PracticeSwiftUI/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  PracticeSwiftUI
+//
+//  Created by Bruce Chen on 2024/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let food = Food.examples
+    @State private var selectedFood: Food?
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            Group {
+                if selectedFood != .none {
+                    Text(selectedFood!.image)
+                        .font(.system(size: 200))
+                        .minimumScaleFactor(0.1)
+                        .lineLimit(1)
+                } else {
+                    Image("dinner")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+            .frame(height: 250)
+            
+            Text("今天吃什麼？")
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood!.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.green)
+                    .id(selectedFood!.name)
+                    .transition(.asymmetric(
+                        insertion: .opacity
+                                    .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                        removal: .opacity
+                                    .animation(.easeInOut(duration: 0.4))))
+            }
+            
+            Spacer()
+            
+            Button {
+                selectedFood = food.shuffled().first { $0 != selectedFood }
+            } label: {
+                Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
+                    .animation(.none, value: selectedFood)
+                    .transformEffect(.identity)
+            }.padding(.bottom, -15)
+            
+            Button {
+                selectedFood = .none
+            } label: {
+                Text("重置").frame(width: 200)
+            }.buttonStyle(.bordered)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.lightGray))
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .animation(.easeInOut(duration: 0.6), value: selectedFood)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeSwiftUI/PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by Bruce Chen on 2024/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
